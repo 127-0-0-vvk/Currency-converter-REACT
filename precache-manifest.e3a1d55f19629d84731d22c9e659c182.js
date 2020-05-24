87b4f60cf094e5831d27abadf397900a4c945348
Vivek <41470324+127-0-0-vvk@users.noreply.github.com>
2020-05-24 13:12:45 +0100

    first commit

---
+++ precache-manifest.e3a1d55f19629d84731d22c9e659c182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5054343c5872341f27ef70ca486b71ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad8691ac650f39ecd518",
+    "url": "/static/css/main.8641062e.chunk.css"
+  },
+  {
+    "revision": "a40e01c2e096edc32d69",
+    "url": "/static/js/2.3cc3962a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3cc3962a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8691ac650f39ecd518",
+    "url": "/static/js/main.89a8fbb1.chunk.js"
+  },
+  {
+    "revision": "64184623ac00b8ac8ae0",
+    "url": "/static/js/runtime-main.b48bf1ae.js"
+  }
+]);
